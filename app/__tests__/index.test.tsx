3f38fcf79031481386bc52ecc546e11e40328894
Prathyusha K <prathyushak@Prathyushas-MacBook-Pro.local>
2025-10-16 10:53:50 +0530

    jest added in the repo

---
+++ app/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react-native";
+import React from "react";
+import Index from "../index";
+
+describe("Index component", () => {
+  it("renders the expected text", () => {
+    const { getByText } = render(<Index />);
+    expect(getByText("Edit app/index.tsx to edit this screen.")).toBeTruthy();
+  });
+});
